32b2fb77a75b9384cb1354df8de343cd21e822b2
Rob Brennan <rob@therobbrennan.com>
2017-12-03 13:37:52 -0800

    Updated example URL

--- www/__examples/jsonRequest.php
+++ www/__examples/jsonRequest.php
@@ -1,5 +1,5 @@
 <?php
-$json_url = "https://bittrex.com/api/v1.1/public/getmarkets";
+$json_url = "https://bittrex.com/api/v1.1/public/getmarketsummaries";
 $json = file_get_contents($json_url);
 $data = json_decode($json, TRUE);
 echo "<pre>";
