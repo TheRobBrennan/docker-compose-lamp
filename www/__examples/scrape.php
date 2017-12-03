56c49b2ebbff194f3d81e3da4f7161bbcddf3f0e
Rob Brennan <rob@therobbrennan.com>
2017-12-03 13:16:45 -0800

    Removed unnecessary code

--- www/__examples/scrape.php
+++
@@ -1,8 +0,0 @@
-<?php
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, "https://www.bittrex.com/");
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_HEADER, 0);
-$output = curl_exec($ch);
-curl_close($ch);
-echo $output;
